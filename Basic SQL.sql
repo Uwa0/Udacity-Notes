2972624c791d9149ba07231281714d13b9c601c4
Uwa0 <113632822+Uwa0@users.noreply.github.com>
2024-05-06 21:28:11 +0300

    Update Basic SQL.sql

--- Basic SQL.sql
+++ Basic SQL.sql
@@ -1,5 +1,24 @@
-Try using LIMIT yourself below by writing a query that displays all the data in the occurred_at, account_id, and channel columns of the web_events table, and limits the output to only the first 15 rows.
+--How SQL commands should be arranged:
+SELECT
+FROM
+ORDER BY
+LIMIT
 
+--LIMIT statement
 SELECT occurred_at, account_id, channel
 FROM web_events
 LIMIT 15
+
+
+--ORDER BY - This clause goes before the LIMIT clause. LIMIT statement always appears last.
+SELECT id, occurred_at,total_amt_usd
+FROM orders
+ORDER BY occurred_at DESC
+LIMIT 10;
+
+--sorting, using ORDER BY on more than one column:
+SELECT id, name, website, sales_rep_id
+FROM accounts
+ORDER BY sales_rep_id DESC, id
+LIMIT 10;
+
