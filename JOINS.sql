cc7fc2134522507393d6fca9767fe9fa0752abbe
Uwa0 <113632822+Uwa0@users.noreply.github.com>
2024-05-12 12:08:03 +0300

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -16,3 +16,24 @@ FROM orders
 JOIN accounts
 ON accounts.id = orders.account_id;
 
+
+--QUESTION: Provide a table for all web_events associated with account name of Walmart. There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event. Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+
+SELECT a.name, a.primary_poc, w.occurred_at, w.channel
+FROM web_events w
+JOIN accounts a
+ON w.account_id = a.id
+WHERE a.name = 'Walmart';
+
+
+--QUESTION: Provide a table that provides the region for each sales_rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+
+SELECT r.name region, s.name sales_rep, a.name account
+FROM sales_reps s
+JOIN region r
+ON s.region_id = r.id
+JOIN accounts a
+ON s.id = a.sales_rep_id
+ORDER BY account;
+
+
