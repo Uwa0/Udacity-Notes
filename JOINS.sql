78c554afb7dba74466ec676b598311d32e3fe6ca
Uwa0 <113632822+Uwa0@users.noreply.github.com>
2024-05-12 10:51:27 +0300

    Update JOINS.sql

--- JOINS.sql
+++ JOINS.sql
@@ -1,1 +1,18 @@
+JOINS
+
+--pulling all columns from both tables
+SELECT accounts.*, orders.*
+FROM accounts
+JOIN orders
+ON accounts.id = orders.account_id;
+
+--Pulling specific columns from both tables
+SELECT orders.standard_qty, 
+  orders.gloss_qty, 
+  orders.poster_qty,
+  accounts.website,
+  accounts.primary_poc
+FROM orders
+JOIN accounts
+ON accounts.id = orders.account_id;
 
